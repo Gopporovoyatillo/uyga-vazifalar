e13b19db20991005f468c042da204ae2000c439c
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-02 20:19:44 +0500

    Add files via upload

---
+++ 2-dars vazifasi.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 19:58:54 2021
+
+@author: Admin
+"""
+
+print('Assalom alaykum') #yozuvli malumot(matn)
+print('Hayrli tong!')    #yozuvli malumot(matn)
+print(2+4*2)             # arifmetik masala
+print(19/5)              # arifmetik masala
+print(2**4)              # darajaga ko'paytirish
+print(2**3)              #  kubini aniqlash
---
+++ 3- PRINT(), SINTEKS VA ARIFMETIK AMALLAR.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 20:08:05 2021
+
+@author: Admin
+"""
+
+# 1-savol) 5 ning 4-darajasini toping
+# 2-savol)22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# 3-savol)Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# 4-savol)Diametri 12 ga teng bo'lgan doiraning yuzini toping  ( deb oling)
+# 5-savol)Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning
+# gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+
+#1 5 ning 4-darajasi
+print('5 ning 4-darajasi', 5**4)
+
+#2 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4 ga bo'lganda qancha qoldiq", 22%4)
+
+#3 Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
+
+#4 Diametri 12 ga teng bo'lgan doiraning yuzini toping
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, 'ga teng')
+
+#5 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
