c393f9f05d25fc5c19fa76569259c863abca0308
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-07 23:59:12 +0500

    Add files via upload

---
+++ 23-dars vazifasi lambda.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 23:21:57 2021
+
+@author: Admin
+"""
+#Topingchi, quyidagi kod qanday vazifani bajaradi?
+#list(filter(lambda meva:(meva.startswith('a') and meva.endswith('r')), mevalar))
+
+
+mevalar = ['olma','anor','anjir','shaftoli',"o'rik","tarvuz","qovun","banan"]
+
+mevalar2=list(filter(lambda meva:(meva.startswith('a') and meva.endswith('r')), mevalar))
+print(mevalar2)
